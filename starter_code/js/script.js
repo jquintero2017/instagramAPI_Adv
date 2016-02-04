506cd02c8524be9c1cc81f34a6bedbf2aa13d171
jquintero2017 <jquintero@cristoreyny.org>
2016-02-04 13:49:12 +0000

    Here's my Flickr API project

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,19 @@
-// YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "36aa5c6b3029e11d9b4daeea8da6276e";
+var userInput = "";
 
 $(document).ready(function() {
-    // your code here
+    $("#search-btn").click(function() {
+        userInput = $("#search-query").val();
+
+        searchFlickr(clientID, userInput, 10, function(photos) {
+            console.log(photos.photo[0].title);
+            for (var i=0; i<photos.photo.length; i++){
+                photos.photo[i].url_s;
+                $("#search-results").append('<img src=' + photos.photo[i].url_s +'>' );
+            }
+        });
+    });
+    $("#reset-btn").click(function(){
+                $("#search-results").empty();
+    });
 });
